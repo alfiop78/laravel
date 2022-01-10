64e3c76ce938626fffad257fa10f4032acd63d7c
Alfio <alfio.p78@gmail.com>
2022-01-10 18:35:28 +0100

    Aggiunte funzionalità al progetto Task

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Task Project - create</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body class="antialiased">
+  <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+
+    <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+      <div class="grid grid-cols-1 md:grid-cols-2">
+        <div class="p-6">
+          <p>Creazione di un nuovo Task</p>
+        </div>
+        <div>
+          <a href="{{ route('tasks.index') }}"></a>
+        </div>
+        <div>
+          <form method="post" action="{{ route('tasks.store') }}">
+            @csrf
+            <div>
+              <label for="taskTitle">Titolo</label>
+              <input type="text" name="taskTitle" placeholder="Title" />
+            </div>
+            <div>
+              <label for="taskDescription">Descrizione</label>
+              <input type="text" name="taskDescription" placeholder="Description" />
+            </div>
+            <button type="submit">Archivia</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,18 @@ Route::get('/', function () {
 
 // Visualizzare tutti i task (GET)
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+
 // Visualizzare il form per la creazione del task (GET)
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
 
 // Archiviare il task (POST)
+Route::post('/tasks/store', [TaskController::class, 'store'])->name('tasks.store');
 
-// Visualizzare uno specifico Task
+// Modificare uno specifico Task
+Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
 
 // Aggiornare il valore del task (PATCH)
+Route::patch('/tasks/update/{id}', [TaskController::class, 'update'])->name('tasks.update');
 
 // Eliminare uno specifico task (DELETE)
+Route::delete('/tasks/{id}', [TaskController::class, 'delete'])->name('tasks.delete');
