3f122591874516e083520be5ecc95cc0d4b5ce1c
Alfio <alfio.p78@gmail.com>
2022-01-10 12:55:49 +0100

    Aggiunta del metodo index nel TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// aggiungo la classe Task del Model
+use App\Models\Task;
 
 class TaskController extends Controller
 {
-    public function index()
-    {
-        return view('tasks.index');
-    }
+  public function index()
+  {
+    // recupero tutti i record dalla tabella tasks
+    $tasks = Task::all();
+    return view('tasks.index')->with('tasks', $tasks);
+  }
 }
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -21,33 +21,28 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+      <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-
-                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                    <div class="grid grid-cols-1 md:grid-cols-2">
-                        <div class="p-6">
-                          <p>Visualizzazione di tutti i task</p>
-                        </div>
-
+            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <div class="grid grid-cols-1 md:grid-cols-2">
+                    <div class="p-6">
+                      <p>Visualizzazione di tutti i task</p>
+                    </div>
+                    <div>
+                      @if ($tasks->count() > 0)
+                        @foreach($tasks as $task)
+                          <p><span>ID : </span><span>{{$task->id}}</span></p>
+                          <p><span>Title : </span><span>{{$task->title}}</span></p>
+                          <p><span>Description : </span><span>{{$task->description}}</span></p>
+                          <p><button type="button">Edit</button></p>
+                        @endforeach
+                      @else
+                          <p>Nessun task disponibile</p>
+                      @endif
                     </div>
                 </div>
-
             </div>
+
         </div>
     </body>
 </html>
